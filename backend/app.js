2746f50ab840d5435f023b743a2fdc090ee6dad4
Kaif Muhammed <kaif@entgra.io>
2022-11-20 01:32:04 +0530

    Chat Crud completed.

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const packageQuestion = require("./routes/PackageQuestionRoute");
 const order = require("./routes/OrderRoute");
 const customerAnswers = require("./routes/CustomerAnswers");
 const mailRouter = require("./routes/MailRouter");
+const chatRoute = require("./routes/ChatRoute");
 
 const app = express();
 
@@ -37,6 +38,8 @@ app.use("/api/order", order);
 app.use("/api/customer-answer", customerAnswers);
 app.use("/api/stripe", stripeRoute);
 app.use("/api/mail", mailRouter);
+app.use("/api/chat", chatRoute);
+// app.use("/api/notification", mailRouter);
 
 //listen
 const port = process.env.PORT || 3300;
